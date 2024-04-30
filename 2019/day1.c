0c83c1b83b1b05dd29b659c316b755526dccc890
Vrindavan Sanap <vrindavansanap@gmail.com>
2024-04-30 11:35:39 +0530

    -

---
+++ 2019/day1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int mass_to_fuel(int mass) { 
+  int fuel = (mass / 3) - 2;
+  if (fuel <= 0){
+    return 0;
+  }else{
+    return fuel + mass_to_fuel(fuel);
+  }
+}
+
+int main() {
+  FILE* file_pointer;
+  char filename[] = "day1.txt";
+  char buffer[20];
+  int total_fuel = 0;
+  file_pointer = fopen(filename, "r");
+  if (file_pointer == NULL) {
+    printf("Unable to open file.\n");
+    return 1;
+  }
+    while (fgets(buffer, sizeof(buffer), file_pointer) != NULL) {
+      int len = (int)strlen(buffer);
+      int mass = atoi(buffer);
+      int fuel = mass_to_fuel(mass);
+      printf("%d \n", fuel);
+      total_fuel += fuel;
+      printf("Total fuel %d \n", total_fuel);
+      
+
+    }
+
+  fclose(file_pointer);
+  printf("Total fuel = %d", total_fuel);
+  return 0;
+}
