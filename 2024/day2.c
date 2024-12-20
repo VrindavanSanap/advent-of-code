b3fc27ce80fcfcf465bf980bf4e2dda0fd886d10
vrindavansanap <vrindavansanap@gmail.com>
2024-12-20 19:59:44 +0000

    -

--- 2024/day2.c
+++ 2024/day2.c
@@ -47,7 +47,7 @@ bool check_gap(int arr[], int size) {
 }
 
 bool safe_unsafe(int arr[], int size) {
- if (!strictly_inc_dec(arr, size)) {
+  if (!strictly_inc_dec(arr, size)) {
 
     return false;
   }
@@ -67,22 +67,24 @@ int main() {
 
   int total_safe = 0;
   int line_i = 0;
+  int size;
   while (fgets(line, sizeof(line), fp)) {
 
-      line_i++;
+    line_i++;
 
     printf("%d ", line_i);
     memset(level, 0, sizeof(level));
     sscanf(line, "%d %d %d %d %d %d %d %d", &level[0], &level[1], &level[2],
            &level[3], &level[4], &level[5], &level[6], &level[7]);
-for (int i = 0; i < size; i++) {
-    printf("%d, ", arr[i]);
-    if (arr[i] == 0) {
-      size = i;
-      break;
+           size = sizeof(level)/sizeof(level[0]);
+    for (int i = 0; i < sizeof(level)/sizeof(level[0]); i++) {
+      printf("%d, ", level[i]);
+      if (level[i] == 0) {
+        size = i;
+        break;
+      }
     }
-  }
- 
+
     bool is_level_safe = safe_unsafe(level, size);
 
     if (is_level_safe) {
