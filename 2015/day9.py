493ed23b11ac6ab2ff794d7fb24240a3ef50d868
vrindavansanap <vrindavansanap@gmail.com>
2025-01-15 09:16:26 +0000

    -

--- 2015/day9.py
+++ 2015/day9.py
@@ -1,6 +1,7 @@
+from copy import deepcopy
 with open("day9_smol.txt", "r") as f:
   lines = f.readlines()
-
+print(lines)
 graph = {}
 for line in lines:
   line = line[:-1]
@@ -9,6 +10,24 @@ for line in lines:
   city1 = line[0]
   city2 = line[2]
   dist = line[4]
-  
-  print(f"City1 = {city1}, City2 = {city2}, dist = {dist}")
-  break
+  if city1 in graph:
+    graph[city1].append((city2, int(dist)))
+  else:
+    graph[city1] = [((city2, int(dist)))]
+
+
+  if city2 in graph:
+    graph[city2].append((city1, int(dist)))
+  else:
+    graph[city2] = [((city1, int(dist)))]
+
+ 
+graph_copy = deepcopy(graph)
+
+for g in (graph):
+  print(g, graph[g])
+
+graph_copy['London'].pop()
+
+
+
