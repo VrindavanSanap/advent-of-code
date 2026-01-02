48116e7b768bdad2f98cfe2c75b61182ffdbfa24
Vrindavan Sanap <vrindavansanap@gmail.com>
2026-01-02 20:05:04 +0530

    Fixed overflow issues

--- 2025/day2/day2.c
+++ 2025/day2/day2.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
-int count_digits(int num){
-  int count = 0;
+unsigned long long count_digits(unsigned long long num){
+  unsigned long long count = 0;
   while (num != 0) {
     num /= 10;
     count++;
   }
   return count;
 }
-int ipow(int num, int power){
-  int result = 1;
-  for (int i = 0; i < power; i++) {
+unsigned long long ipow(unsigned long long num, unsigned long long power){
+  unsigned long long result = 1;
+  for (unsigned long long i = 0; i < power; i++) {
     result *= num;
   }
   return result;
 
 }
 
-bool check_valid(int num){
+bool check_valid(unsigned long long num){
   /*
   Returns true if num is valid 
   Returns false if num is invalid
@@ -28,9 +28,9 @@ bool check_valid(int num){
     return true;
   }
 
-  int divisor = ipow(10, (num_digits / 2));
-  int first = num / divisor;
-  int second = num % divisor;
+  unsigned long long divisor = ipow(10, (num_digits / 2));
+  unsigned long long first = num / divisor;
+  unsigned long long second = num % divisor;
   if (first == second){
     return false;
   }
@@ -39,7 +39,7 @@ bool check_valid(int num){
 }
 
 int main() {
-  char* file_name = "day2_smol.txt";
+  char* file_name = "day2.txt";
   FILE* fp = fopen(file_name, "r");
   if (fp == NULL) {
     printf("Failed to read the file");
@@ -51,23 +51,23 @@ int main() {
   size_t len;
 
   int n_invalid = 0;
-  unsigned long long invalid_sum = 0;
+  unsigned long long invalid_sum = 0ULL;
   while ((buffer_size = getdelim(&buffer, &len, ',', fp)) != -1) {
     // buffer[buffer_size - 1] = '\0';
     printf("%s\n", buffer);
-    int start_num;
-    int end_num;
-    if (sscanf(buffer, "%d-%d", &start_num, &end_num) == 2) {
-      printf("%d --> %d\n", start_num, end_num);
+    unsigned long long start_num;
+    unsigned long long end_num;
+    if (sscanf(buffer, "%llu-%llu", &start_num, &end_num) == 2) {
+      printf("%llu --> %llu\n", start_num, end_num);
     } else {
       printf("Parsing failed.\n");
     }
-    for (int i = start_num; i <= end_num; i++) {
+    for (unsigned long i = start_num; i <= end_num; i++) {
       bool is_not_valid = !check_valid(i);
       if (is_not_valid){
         invalid_sum += i;
         n_invalid++;
-        printf("%d -> Invalid\n", i);
+        printf("%lu -> Invalid\n", i);
       }else{
         // printf("%d -> Valid\n", i);
       }
