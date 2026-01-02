85725e528101e26e7c9e5a3ed60ef12d0c47012c
Vrindavan Sanap <vrindavansanap@gmail.com>
2026-01-02 22:33:33 +0530

    day3 part1 done

--- 2025/day3/day3.c
+++ 2025/day3/day3.c
@@ -2,8 +2,42 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int index_of_max_digit(char* given_string, int start_index, int end_index) {
+  int max_digit = 0;
+  int max_index = 0;
+  for (int i = start_index; i <= end_index; i++) {
+    if (given_string[i] > max_digit) {
+      max_digit = given_string[i];
+      max_index = i;
+    }
+  }
+  return max_index;
+}
+int get_max_joltage(char* bank) {
+  int max_joltage = 0;
+  int last_index = strlen(bank) - 1;
+
+  int max_digit_index = index_of_max_digit(bank, 0, last_index);
+  int second_max_digit_index;
+  if (max_digit_index != last_index) {
+    second_max_digit_index =
+        index_of_max_digit(bank, max_digit_index + 1, last_index);
+  } else {
+    second_max_digit_index = index_of_max_digit(bank, 0, last_index - 1);
+    int temp = max_digit_index;
+    max_digit_index = second_max_digit_index;
+    second_max_digit_index = temp;
+  }
+  char max_joltage_str[3] = {bank[max_digit_index], bank[second_max_digit_index],
+                         '\0'};
+
+  max_joltage = atoi(max_joltage_str);
+  return max_joltage;
+}
 int main() {
-  char* file_name = "day3_smol.txt";
+  char* file_name = "day3.txt";
   char* line = NULL;  // Pointer to the buffer
   size_t len = 0;     // Size of the allocated buffer
   ssize_t read;       // Number of characters read
@@ -13,13 +47,18 @@ int main() {
     perror("Error opening file");
     return EXIT_FAILURE;
   }
+  int sum_of_joltages = 0;
 
   while ((read = getline(&line, &len, fp)) != -1) {
-    printf("Retrieved line of length %zu:\n", read);
-    printf("%s", line);
+    line[strcspn(line, "\n")] = '\0';
+    printf("%s \n", line);
+    printf("max_joltage-> %d \n", get_max_joltage(line));
+    sum_of_joltages += get_max_joltage(line);
+
   }
 
   free(line);
+  printf("sum_of_joltages-> %d \n", sum_of_joltages);
   fclose(fp);
   fclose(fp);
 
