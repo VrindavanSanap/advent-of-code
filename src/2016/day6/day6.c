3f48719274024648bb42881322efbfa05060c2a8
Vrindavan Sanap <vrindavansanap@gmail.com>
2026-01-09 14:39:35 +0530

    day6 done

---
+++ src/2016/day6/day6.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	char* file_name = "day6.txt";
+	FILE* fp = fopen(file_name, "r");
+	if (fp == NULL) {
+		printf("Error opening file %s\n", file_name);
+		return 1;
+	}
+	char* line = NULL;
+	size_t len = 0;
+	int freq[26][8]={0};
+	while(getline(&line, &len, fp) != EOF){
+		for (int i = 0; i < 8; i++){
+			freq[line[i] - 'a'][i]++;
+		}
+	}
+	for (int i = 0; i < 8; i++){
+		int min= 999;
+		int min_index = 0;
+		for (int j = 0; j < 26; j++){
+			if (freq[j][i] < min){
+				min = freq[j][i];
+				min_index = j;
+			}
+		}
+		printf("%c", min_index + 'a');
+	}
+	free(line);
+	fclose(fp);
+}
