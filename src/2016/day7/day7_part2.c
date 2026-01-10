24d511ec9390ca06cb042157417c45adec7ca88c
Vrindavan Sanap <vrindavansanap@gmail.com>
2026-01-10 12:37:13 +0530

    boilerplate

--- src/2016/day7/day7_part2.c
+++ src/2016/day7/day7_part2.c
@@ -2,31 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "dynamic_array.h"
-dynamic_array* get_abas(char* line){
+dynamic_array* get_abas(char* line) {
+  dynamic_array* abas = da_build(sizeof(char[3]));
+  size_t len = strlen(line);
+  // returns abas outside the hypernet sequence
 
-	dynamic_array* abas = da_build(sizeof(char[3]));
-	size_t len = strlen(line);
-	// returns abas outside the hypernet sequence
-
-	return abas;
+  return abas;
 }
-dynamic_array* get_babs(char* line){
-	dynamic_array* babs = da_build(sizeof(char[3]));
-	size_t len = strlen(line);
-	// returns babs inside the hypernet sequence
+dynamic_array* get_babs(char* line) {
+  dynamic_array* babs = da_build(sizeof(char[3]));
+  size_t len = strlen(line);
+  // returns babs inside the hypernet sequence
 
-	return babs;
+  return babs;
 }
 bool validate_line(char* line) {
   int is_valid = false;
   // returns true if there a corresponding bab in the hypernet sequence
   // for the aba in the non hypernet sequence
   dynamic_array* abas = get_abas(line);
+  return is_valid;
   dynamic_array* babs = get_babs(line);
   da_free(abas);
   da_free(babs);
-  return is_valid;
 }
 int main() {
   char* file_name = "day7.txt";
@@ -39,9 +39,12 @@ int main() {
   size_t size = 0;
 
   int total_valid = 0;
-  while (getline(&line, &size, file) != -1) {
-    // printf("%s\n", line);
-    total_valid += validate_line(line);
-  }
-  printf("%d 0", total_valid);
+  char* test_line = "zazbz[bzb]cdb";
+  total_valid += validate_line(test_line);
+  printf("%d\n", total_valid);
+  // while (getline(&line, &size, file) != -1) {
+  //   total_valid += validate_line(line);
+  // }
+
+  // printf("%d 0", total_valid);
 }
