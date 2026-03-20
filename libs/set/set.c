f870494d360f1ea58780e62eb58bba89ef462075
Vrindavan Sanap <vrindavansanap@gmail.com>
2026-03-20 16:05:04 +0530

    add set

---
+++ libs/set/set.c
@@ -0,0 +1,80 @@
+#include "set_interface.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "dynamic_array.h"
+struct set {
+  dynamic_array *elements;
+  int (*compare)(const void *a, const void *b);
+};
+
+set *set_build(size_t element_size,
+               int (*compare)(const void *a, const void *b)) {
+  if (element_size == 0) {
+    fprintf(stderr, "Error : element_size cannot be 0\n");
+    return NULL;
+  }
+  if (!compare) {
+    fprintf(stderr, "Error: compare function cannot be NULL\n");
+    return NULL;
+  }
+  set *s = malloc(sizeof(*s));
+  if (!s) {
+    fprintf(stderr, "Error: Set allocation failed\n");
+    return NULL;
+  }
+  dynamic_array *elements = da_build(element_size);
+  if (!elements) {
+    fprintf(stderr, "Error: Set elements array allocation failed\n");
+    free(s);  // Prevent memory leak!
+    return NULL;
+  }
+
+  *s = (set){.elements = elements, .compare = compare};
+
+  return s;
+}
+void set_insert(set *s, const void *element) {
+  if (!s || !element) return;
+  // insert only if the set does not already have the element
+  if (set_find(s, element)) {
+    return;
+  }
+  da_insert_last(s->elements, element);
+}
+bool set_find(set *s, const void *element) {
+  // returns true if the element exists in the set
+  size_t element_size = get_element_size(s->elements);
+  size_t num_elements = da_get_size(s->elements);
+  for (size_t i = 0; i < num_elements; i++) {
+    char temp_buffer[element_size];
+    da_get_at(s->elements, i, temp_buffer);
+    if (s->compare(temp_buffer, element) == 0) {
+      return true;
+    }
+  }
+  return false;
+}
+void set_delete(set *s, const void *element) {
+  if (!s || !element) return;
+  size_t element_size = get_element_size(s->elements);
+  for (size_t i = 0; i < da_get_size(s->elements); i++) {
+    char temp_buffer[element_size];
+    da_get_at(s->elements, i, temp_buffer);
+    if (s->compare(temp_buffer, element) == 0) {
+      da_delete_at(s->elements, i);
+      return;
+    }
+  }
+}
+size_t set_get_size(set *s) { return da_get_size(s->elements); }
+void set_free(set *s) {
+  if (!s) {
+    return;
+  }
+  da_free(s->elements);
+
+  free(s);
+}
---
+++ libs/set/set.h
@@ -0,0 +1,53 @@
+/*
+  Set interface implementation using dynamic-array datastruture
+  Operations
+    All these operatoins have time complexity of O(n)
+    Container
+      build(A) :build a set given iterable A
+    Static
+      find(k)
+    Dynamic
+      insert(k)
+      delete(k)
+    Order
+      find_min()
+      find_max()
+      find_prev(k)
+      find_next(k)
+*/
+
+#include <stdint.h>
+
+#include "dynamic_array.h"
+#include "stdbool.h"
+
+typedef struct set set;
+
+// Container
+/*
+  cmp_func:
+    returns 0 if a and b are equal
+    returns a positive value if a > b
+    returns a negative value if a < b
+*/
+set *set_build(size_t element_size,
+               int (*compare)(const void *a, const void *b));
+
+// Static
+bool set_find(set *s, const void *element);
+size_t set_get_size(set *s);
+
+// Dynamic
+void set_insert(set *s, const void *element);
+void set_delete(set *s, const void *element);
+
+// Order
+/*
+TODO
+void find_min(set *s, void *out_key);
+void find_max(set *s, void *out_key);
+void find_prev(set *s, void *in_key, void *out_key);
+void find_prev_entry(set *s, uint64_t hash, void *out_key);
+void find_next(set *s, void *in_key, void *out_key);
+void find_next_entry(set *s, uint64_t hash, void *out_key);
+*/
