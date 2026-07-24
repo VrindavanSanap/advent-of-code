5aa42e0c9ac15b4ce898162bf9c82d1f818d16da
Vrindavan Sanap <66788089+VrindavanSanap@users.noreply.github.com>
2026-07-24 16:08:05 +0530

    added the c++ version

--- src/2020/day8/day8.cc
+++ src/2020/day8/day8.cc
@@ -1,11 +1,45 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <map>
+#include <regex>
 #include <string>
+
 using namespace std;
-int main() { 
-	fstream file("2020_day8.txt");
+int main() {
+  fstream file("2020_day8.txt");
   string line;
-  while(getline(file, line)){
-    cout << line << endl;
+  int accumulator = 0;
+  map<int, tuple<string, bool>> lines;
+  int num_lines = 0;
+  while (getline(file, line)) {
+    lines.emplace(num_lines, make_tuple(line, false));
+    num_lines++;
   }
+
+  int ip = 0;
+  while (true) {
+    auto &[line, visited] = lines[ip];
+    if (visited) {
+      break;
+    } else {
+      visited = true;
+    }
+    string instruction;
+    int argument;
+    static const regex pattern(R"(([a-zA-Z]+)\s([-+]?\d+))");
+    smatch matches;
+    regex_search(line, matches, pattern);
+    instruction = matches[1].str();
+    argument = stoi(matches[2].str());
+    if (instruction == "nop") {
+    } else if (instruction == "acc") {
+      accumulator += argument;
+    } else if (instruction == "jmp") {
+      ip += argument;
+      continue;
+    }
+    ip++;
+  }
+  cout << accumulator << endl;
+  return 0;
 }
