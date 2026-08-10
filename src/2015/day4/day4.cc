27138a7234187af04304e3a5839be17773312ebf
Vrindavan Sanap <66788089+VrindavanSanap@users.noreply.github.com>
2026-08-10 15:36:17 +0530

    c++ version

--- src/2015/day4/day4.cc
+++ src/2015/day4/day4.cc
@@ -1,21 +1,35 @@
-#include <stdio.h>
-#include <string.h>
-
 #include <CommonCrypto/CommonDigest.h>
 
-int main()
-{
-    unsigned char digest[CC_MD5_DIGEST_LENGTH];
-    const char string[] = "pqrstuv1048970";
+#include <iostream>
+#include <string>
+double nanos(void) {
+  struct timespec ts;
 
-    CC_MD5_CTX context;
-    CC_MD5_Init(&context);
-    CC_MD5_Update(&context, string, (CC_LONG)strlen(string));
-    CC_MD5_Final(digest, &context);
-
-    for (size_t i=0; i<1; ++i)
-        printf("%.2x", digest[i]);
-    fputc('\n', stdout);
+  if (timespec_get(&ts, TIME_UTC) != TIME_UTC) {
+    fputs("timespec_get failed!", stderr);
     return 0;
+  }
+  return 1000000000.0 * ts.tv_sec + ts.tv_nsec;
 }
+using namespace std;
+int main() {
+  double st = nanos();
+  unsigned char digest[CC_MD5_DIGEST_LENGTH];
+  int i = 0;
+  while (true) {
+    string secret_key = "ckczppom";
+    secret_key += to_string(i);
+    CC_LONG len = (CC_LONG)secret_key.size();
+    CC_MD5(secret_key.c_str(), len, digest);
+    if (digest[0] == 0 && digest[1] == 0 && digest[2] == 0) {
+      cout << i << endl;
+      break;
+    }
+    i++;
+  }
+  double et = nanos();
+  double s = ((et - st) * 1e-9);
+  printf("Time taken %f secs", (s));
 
+  return 0;
+}
