870578afb5dd8367866052a4482137ef08e6356e
Vrindavan Sanap <66788089+VrindavanSanap@users.noreply.github.com>
2026-08-14 15:59:05 +0530

    cpp version

---
+++ src/2015/day6/day6_part2.cc
@@ -0,0 +1,47 @@
+#include <ATen/ATen.h>
+
+#include <fstream>
+#include <iostream>
+#include <regex>
+
+using namespace std;
+int main() {
+  auto opts = at::TensorOptions().dtype(at::kChar);
+  at::Tensor lights = at::zeros({1000, 1000}, opts);
+  ifstream file("2015_day6.txt");
+  if (!file.is_open()) {
+    cerr << "Error: could not open file" << endl;
+    return 1;
+  }
+  string line;
+  regex pattern(R"((on|off|toggle)\s(\d+),(\d+)\D+(\d+),(\d+))");
+
+  smatch matches;
+
+  while (getline(file, line)) {
+    regex_search(line, matches, pattern);
+    int x1 = stoi(matches[2]);
+    int y1 = stoi(matches[3]);
+    int x2 = stoi(matches[4]);
+    int y2 = stoi(matches[5]);
+    if (matches[1] == "toggle") {
+      lights.slice(0, x1, x2 + 1).slice(1, y1, y2 + 1).add_(2);
+    }
+    if (matches[1] == "on") {
+
+      lights.slice(0, x1, x2 + 1).slice(1, y1, y2 + 1).add_(1);
+    }
+    if (matches[1] == "off") {
+      lights.slice(0, x1, x2 + 1)
+          .slice(1, y1, y2 + 1)
+          .sub_(1)
+          .clamp_min_(0);
+    }
+  }
+
+  file.close();
+  int64_t total = lights.sum().item<int64_t>();
+  cout << total << endl;
+
+  return 0;
+}
