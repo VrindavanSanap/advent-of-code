06b6973733659491be77b28f00e2bc7dca6067e8
Vrindavan Sanap <66788089+VrindavanSanap@users.noreply.github.com>
2026-08-20 09:16:36 +0530

    standardisation

--- src/2022/day2/day2.c
+++ src/2022/day2/day2.c
@@ -1,37 +1,66 @@
-#include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-#include "minmax.h"
+int get_score(char *line) {
+  //  printf("%c %c ",line[0], line[2]);
+  // A B C
+  // R P S
+  // X Y Z
+  // 1 2 3
+  int their_move = line[0] - 65;
+  int your_move = line[2] - 88;
+  char rps[3] = {'R', 'P', 'S'};
+  printf("%c -vs- %c \n", rps[your_move], rps[their_move]);
+
+  int game_res_arr[3][3] = {
+      {0, -1, 1},  // Your R vs Their R P S
+      {1, 0, -1},  // Your P vs Their R P S
+      {-1, 1, 0}   // Your S vs Their R P S
+  };
+  int game_res = game_res_arr[your_move][their_move];
+  int total_score = 1;
+  total_score += your_move;
+  if (game_res > 0) {
+    printf("You won! YAY \n");
+    total_score += 6;
+  }
+  if (game_res == 0) {
+    printf("Game draw \n");
+    total_score += 3;
+  }
+  if (game_res < 0) {
+    printf("You Lost :( \n");
+  }
+  return total_score;
+}
 
 int main() {
+  int win_pts = 6;
+  int draw_pts = 3;
+  int lose_pts = 0;
+
+  int rock_pts = 1;
+  int paper_pts = 2;
+  int scissors_pts = 3;
+
   FILE *file;
-  char calorie_string[100];
+  char line[10];
 
-  file = fopen("2022_day1.txt", "r");
-
+  file = fopen("2022_day2.txt", "r");
   if (file == NULL) {
-    perror("Error opening file");
+    printf("Could not open the file.\n");
     return 1;
   }
 
-  int most_calories = 0;
-  int total_elf_calories = 0;
-  int elf_number = 1;
-  while (fgets(calorie_string, sizeof(calorie_string), file) != NULL) {
-    printf("%s", calorie_string);
-    int calorie_int = atoi(calorie_string);
-    total_elf_calories += calorie_int;
-    if (!calorie_int) {
-      most_calories = max(total_elf_calories, most_calories);
-      printf("Elf %d, Total calories %d, Most calories %d \n", elf_number,
-             total_elf_calories, most_calories);
-      printf("------\n");
-      total_elf_calories = 0;
-      elf_number += 1;
-    }
+  int count = 0;
+  int total_score = 0;
+  get_score(line);
+  while (fgets(line, sizeof(line), file) != NULL) {
+    printf("%d\n", ++count);
+    printf("%s", line);
+    int score = get_score(line);
+    total_score += score;
+    printf("score: %d total: %d\n", score, total_score);
   }
+
   fclose(file);
-  return 0;
 }
--- src/2022/day2/day2.py
+++ src/2022/day2/day2.py
@@ -1,15 +1,28 @@
-with open("day1.txt") as f:
-  number_strings = f.readlines()
+#!/usr/bin/python3
+with open("day2.txt") as f:
+  lines = f.readlines()
 
-s = 0 
-sum_list = []
-for i in number_strings:
-  if i != "\n":
-    i = int(i)
-    s += int(i)
-  else:
-    sum_list.append(s)
-    s = 0 
+def result(your, their):
+  results = [[0, -1, 1], # R | R P S
+             [1, 0, -1], # P | R P S
+             [-1, 1, 0]] # S | R P S
+  return results[your][their]
+total_score = 0 
+count = 0 
+for line in lines:
+  score = 0 
+  their = ord(line[0]) - 65
+  your = ord(line[2]) - 65 - 26 + 3
+  score += your + 1
+  res = result(your, their)
 
-print(((sorted(sum_list))[-3:]))
-print(sum((sorted(sum_list))[-3:]))
+  if res == 1: # you won
+    score += 6
+
+  elif res == 0:
+    score += 3
+  count += 1 
+  print(f"shape score {your + 1} match score {score - your - 1} total score {score}")
+  print(count, your, their, score)
+  total_score += score
+  print(total_score)
