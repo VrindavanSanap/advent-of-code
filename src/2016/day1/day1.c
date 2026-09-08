bdb1b04c3f965c6b2042f819391e7e2e39264c54
Vrindavan Sanap <66788089+VrindavanSanap@users.noreply.github.com>
2026-09-08 23:00:14 +0530

    c impl

--- src/2016/day1/day1.c
+++ src/2016/day1/day1.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int mod(int a, int n) {
+  int r = a % n;
+  if (r < 0) {
+    r += n;
+  }
+  return r;
+}
 
 int main() {
-  FILE *fptr;
-  fptr = fopen("2016_day1.txt", "r");
-  int direction[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
-  char *step;
-  token = strtok(str, ",");
+  FILE *file = fopen("./2016_day1.txt", "r");
+  int x = 0;
+  int y = 0;
+  char *data = NULL;
+  size_t size = 0;
+  getline(&data, &size, file);
+  char *token = strtok(data, ", ");
+  int directions[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+  int current_dir = 0;
   while (token != NULL) {
-    printf("%s\n", token);
-    token = strtok(NULL, ",");
+    if (token[0] == 'R') {
+      current_dir += 1;
+    } else if (token[0] == 'L') {
+      current_dir -= 1;
+    }
+    current_dir = mod(current_dir, 4);
+    int d = atoi(token + 1);
+    x += directions[current_dir][0] * d;
+    y += directions[current_dir][1] * d;
+    token = strtok(NULL, ", ");
   }
+  int dist = abs(x) + abs(y);
+  printf("%d\n", dist);
+  free(data);
+  fclose(file);
   return 0;
 }
